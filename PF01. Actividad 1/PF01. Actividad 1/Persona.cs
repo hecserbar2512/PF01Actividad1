fd1b52bf683f6d5345a631aeaa1018f905721352
Hector Serna <hectorsenar3@gmail.com>
2026-09-21 19:46:40 +0200

    Creacion clase Persona

---
+++ PF01. Actividad 1/PF01. Actividad 1/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Persona
+{
+	private string _nombre;
+	private int _edad;
+	private bool _estadoCivil;
+
+	public Persona(string nombre, int edad, bool estadoCivil)
+	{
+		_nombre = nombre;
+		_edad = edad;
+		_estadoCivil = estadoCivil;
+	}
+
+	public string MostrarDatos()
+	{
+		string datos = $"Nombre: {_nombre} \n Edad: {_edad} Estado civil: ";
+
+		if (EstadoCivil())
+		{
+			datos += "Casado";
+
+		}
+		else
+		{
+			datos += "Soltero";
+		}
+		return datos;
+	}
+	public bool EstadoCivil()
+    {
+		if (_estadoCivil)
+		{
+			return true
+		}
+		return false
+    }
+}
