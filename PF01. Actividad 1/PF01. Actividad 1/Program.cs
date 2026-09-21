16f127187659bb91ee685eee3fa91773b3a8c14b
Hector Serna <hectorsenar3@gmail.com>
2026-09-21 18:59:24 +0200

    Creacion del proyecto

---
+++ PF01. Actividad 1/PF01. Actividad 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PF01._Actividad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
