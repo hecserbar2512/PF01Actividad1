b76071220e3c62e6a8140958c0e43e8a545be2ba
Alejandro Sánchez Cordero <alesancor7@alu.edu.gva.es>
2026-09-24 19:27:06 +0200

    Creación clase Producto

---
+++ PF01. Actividad 1/PF01. Actividad 1/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PF01._Actividad_1
+{
+    public class Producto
+    {
+        private string _nombre;
+        private double _precio;
+
+    }
+}
