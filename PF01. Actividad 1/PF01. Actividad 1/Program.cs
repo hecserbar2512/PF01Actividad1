d27dad4cbb73951019a9dc216555dc29860595e6
Alejandro Sánchez Cordero <alesancor7@alu.edu.gva.es>
2026-09-25 19:19:38 +0200

    LLamada a la función desde el main

--- PF01. Actividad 1/PF01. Actividad 1/Program.cs
+++ PF01. Actividad 1/PF01. Actividad 1/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Persona persona = new Persona("David", 25, true);
+            persona.EsMayorDeEdad();
         }
     }
 }
